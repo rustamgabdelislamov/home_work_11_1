069fdd84f617c31adec057e76b789d14b7bc7b4c
Rustam Gabdelislamov <rustamgabdelislamov911@gmail.com>
2025-05-07 23:11:26 +0500

    add transaction_descriptions

--- generators/generators.py
+++ generators/generators.py
@@ -2,3 +2,10 @@ def filter_by_currency(transactions, currency_code):
     for transaction in transactions:
         if transaction['operationAmount']['currency']['code'] == currency_code:
             yield transaction
+
+
+def transaction_descriptions(transactions):
+    for transaction in transactions:
+       yield transaction["description"]
+
+
--- generators/main.py
+++
@@ -1,7 +0,0 @@
-from generators.generators import filter_by_currency
-
-transactions = input()
-
-usd_transactions = filter_by_currency(transactions, "USD")
-for _ in range(2):
-    print(next(usd_transactions))
