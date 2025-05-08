f219ec4d933f087f113921974d0868633341812e
Rustam Gabdelislamov <rustamgabdelislamov911@gmail.com>
2025-05-08 11:50:27 +0500

    add card_number_generator

--- generators/generators.py
+++ generators/generators.py
@@ -9,3 +9,8 @@ def transaction_descriptions(transactions):
        yield transaction["description"]
 
 
+def card_number_generator(start, end):
+    for number in range(start, end):
+        yield f"{number // 1000000000000000:04} {number // 100000000000 % 10000:04} " \
+              f"{number // 10000 % 10000:04} {number % 10000:04}"
+
